906d15f38a37997f22f37493e7f80fe851cd1da6
Jordan <jordanlindgren@live.com>
2021-10-28 17:40:20 -1000

    fixed path issues with front end code added server.js backend API

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs/promises");
 
 const app = express();
 const PORT = 3001;
-
+console.log("configuring express");
 app.use(express.static("public"));
+
+app.get("/api/notes", async (req, res) => {
+  let notes = await fs.readFile("./db/db.json");
+
+  res.json(notes);
+});
+
+app.listen(PORT, () =>
+  console.log(`Test Example app listening at http://localhost:${PORT}`)
+);
